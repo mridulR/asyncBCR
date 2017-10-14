c201e059c3ddd6a5935cd14a584b70cfe5b78842
mridul <mailtoranjanmridul@gmail.com>
2017-10-14 04:30:06 -0400

    Shuttle passing to and fro

--- src/olympus/olympus.py
+++ src/olympus/olympus.py
@@ -2,19 +2,21 @@
 import da
 _config_object = {}
 import sys
-from configparser import ConfigParser
+import subprocess
+import time
+from src.replicas.replica import replica
 
-class Node_(da.NodeProcess):
+class olympus(da.DistProcess):
 
     def __init__(self, procimpl, props):
         super().__init__(procimpl, props)
         self._events.extend([])
 
+    def setup(self, config_file, **rest_186):
+        super().setup(config_file=config_file, **rest_186)
+        self._state.config_file = config_file
+        self._state.config_file = self._state.config_file
+
     def run(self):
-        parser = ConfigParser()
-        config_file = 'config/basic_configuration.properties'
-        if (len(sys.argv) == 2):
-            config_file = sys.argv[1]
-            print(('Config file : ' + config_file))
-        parser.read(config_file)
-        print(parser.get('olympus', 'timeout_in_sec'))
+        print('In Olympus Run ')
+        'for ind in range(1, 4):\n            print(str(ind) + "************************")\n            node_name = \'ReplicaNode\' + str(ind)\n            \n            #cmd = \'python3 -m da -n \'+ node_name + \' -D -f --logfilename logs/\' + node_name             #    + \'.log\' + \' -L info \' + \' src/replicas/replica.da \' + config_file + \' \' + node_name\n            #subprocess.Popen(cmd, shell= True)\n            #print("subprocess ran for ", node_name)\n            #time.sleep(1)\n            output("started node " + node_name)\n\n            if ind == 1:\n                rep = new(replica, args = ("ReplicaNode1", "ReplicaNode1",                     "ReplicaNode2", "ReplicaNode3", ), at = "ReplicaNode1")\n                start(rep)\n            elif ind == 2:\n                rep = new(replica, args = ("ReplicaNode1", "ReplicaNode2",                     "ReplicaNode3", "ReplicaNode3", ), at = "ReplicaNode2")\n                start(rep)\n            else:\n                rep = new(replica, args = ("ReplicaNode1", "ReplicaNode3",                     "None", "ReplicaNode3", ), at = "ReplicaNode3")\n                start(rep)'
--- src/tester.py
+++
@@ -1,4 +0,0 @@
-from utils.config_parser import print_config
-
-if __name__ == "__main__":
-    print_config("../config/config.csv");
