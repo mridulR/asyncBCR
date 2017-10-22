1ac93cc8845140d2740b8a53e9d7f21c7c195bb4
mridul <mailtoranjanmridul@gmail.com>
2017-10-21 21:42:10 -0400

    Added command executor for running command

---
+++ src/utils/command_executor.py
@@ -0,0 +1,66 @@
+import re
+
+def parse_order_string(order_string):
+    comm = -1
+    arg1 = None
+    arg2 = None
+    arg3 = None
+    if order_string.startswith('put'):
+        comm = 1
+        both_arg  = re.search(r'\((.*?)\)',order_string).group(1)
+        arguments = [each for each in both_arg.split(',')]
+        arg1 = arguments[0].replace("'", "")
+        arg2 = arguments[1].replace("'", "")
+    elif order_string.startswith('get'):
+        comm = 2
+        arg1 = re.search(r'\((.*?)\)',order_string).group(1).replace("'", "")
+    elif order_string.startswith('slice'):
+        comm = 3
+        all_arg = re.search(r'\((.*?)\)',order_string).group(1)
+        arg1_pair = [each for each in all_arg.split(',')]
+        arg1 = arg1_pair[0].replace("'", "")
+        arg2_3 = [each for each in arg1_pair[1].split(':')]
+        arg2 = arg2_3[0].replace("'", "")
+        arg3 = arg2_3[1].replace("'", "")
+    elif order_string.startswith('append'):
+        comm = 4
+        both_arg  = re.search(r'\((.*?)\)',order_string).group(1)
+        arguments = [each for each in both_arg.split(',')]
+        arg1 = arguments[0].replace("'", "")
+        arg2 = arguments[1].replace("'", "")
+    return comm, arg1, arg2, arg3
+
+
+def get_result(order_string, running_state):
+    # command parsing 
+    #  1 - >  put (arg1, arg2)
+    #  2 - >  get (arg1)    arg2 is None
+    #  3 - >  slice (arg1, arg2:arg3)
+    #  4 - >  append (arg1, arg2)
+    comm, arg1, arg2, arg3 = parse_order_string(order_string)
+    result = ""
+    if comm == 1:
+        running_state[arg1] = arg2
+        result = "OK"
+    elif comm == 2:
+        if arg1 in running_state:
+            result = running_state[arg1]
+    elif comm == 3:
+        if arg1 in running_state:
+            curr_val = running_state[arg1]
+            if int(arg2) >= 0 and int(arg3) <= len(curr_val) and int(arg2) <= int(arg3):
+                running_state[arg1] = curr_val[ int(arg2) : int(arg3) ]
+                result = "OK"
+        else:
+            result = "FAIL"
+    elif comm == 4:
+        if arg1 in running_state:
+            curr_value = running_state[arg1]
+            running_state[arg1] = curr_value + arg2
+            result = "OK"
+        else:
+            result = "FAIL"
+    else:
+        print("Unrecognized command : ", order_string )
+        return None
+    return result
